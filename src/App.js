70773b65bfc5dcdc9b4c90657e7f814cce580014
AdornChoga <adornchoga@gmail.com>
2022-03-10 08:24:40 +0200

    Add date filter

--- src/App.js
+++ src/App.js
@@ -2,19 +2,21 @@ import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Views from './Views/Views';
 import { actionCreators } from './store/statistics/statistics';
+import Filter from './components/filters/Filter';
 
 const App = () => {
-  const { loading } = useSelector((state) => state.statistics);
+  const { loading, fetchError, filtering } = useSelector((state) => state.statistics);
   const dispatch = useDispatch();
   const { statistics } = useSelector((state) => state);
   useEffect(() => {
     if (statistics.countries.length === 0) {
-      dispatch(actionCreators.initializeState());
+      dispatch(actionCreators.setAppStore());
     }
   }, []);
   return (
     <>
       { loading ? <p>Getting data...</p> : <Views /> }
+      { filtering || fetchError ? <Filter fetchError={fetchError} /> : '' }
     </>
   );
 };
---
+++ src/components/filters/Filter.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import PropTypes from 'prop-types';
+import { FaExclamationCircle } from 'react-icons/fa';
+import { actionCreators } from '../../store/statistics/statistics';
+import classes from './Filter.module.css';
+
+const Filter = (props) => {
+  const { fetchError } = props;
+  const [error] = useState(fetchError);
+  const [input, setInput] = useState('');
+  const [dates, setDates] = useState({ currentDate: input, to: '', from: '' });
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+  }, [error]);
+
+  const handleDateChange = (e) => {
+    setInput(e.target.value);
+    setDates({ ...dates, currentDate: e.target.value });
+  };
+
+  const handleSubmission = async (e) => {
+    e.preventDefault();
+    if (dates.currentDate !== '') {
+      dispatch(actionCreators.filterByOneDate(dates.currentDate));
+      dispatch(actionCreators.setAppStore(dates.currentDate, 1));
+      if (!error) {
+        dispatch(actionCreators.stopFiltering());
+      }
+    }
+  };
+
+  return (
+    <div className={classes.container}>
+      { error ? (
+        <h5 className={classes.fetch_error_msg}>
+          <FaExclamationCircle className={classes.info_icon} />
+          Sorry, could not get data for that date
+        </h5>
+      )
+        : ''}
+      <form>
+        <input type="date" value={input} className={classes.current_date} onChange={handleDateChange} />
+        <br />
+        <button type="submit" onClick={handleSubmission} className={classes.submit}>
+          Get Statistics
+        </button>
+      </form>
+    </div>
+  );
+};
+
+Filter.propTypes = {
+  fetchError: PropTypes.bool.isRequired,
+};
+
+export default Filter;
